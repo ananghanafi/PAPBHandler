833f8d8e45d8a80c79125a961ec57fb8c7464d19
AnangHanafi <ananghanafi>
2016-10-08 09:45:49 +0700

    Update Handler

--- app/src/main/java/com/masbie/papbhandler/MainActivity.java
+++ app/src/main/java/com/masbie/papbhandler/MainActivity.java
@@ -13,19 +13,165 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ToggleButton toggle = (ToggleButton) findViewById(R.id.tgbutton);
-        final ImageView image = (ImageView) findViewById(R.id.image1);
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        final ToggleButton[] toggle = new ToggleButton[11];
+        toggle[0] = (ToggleButton) findViewById(R.id.tgbutton);
+        toggle[1] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[2] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[3] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[4] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[5] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[6] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[7] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[8] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[9] = (ToggleButton) findViewById(R.id.tgbutton1);
+        toggle[10] = (ToggleButton) findViewById(R.id.tgbutton1);
+        final ImageView[] image = new ImageView[11];
+        image[0] = (ImageView) findViewById(R.id.image);
+        image[1] = (ImageView) findViewById(R.id.image1);
+        image[2] = (ImageView) findViewById(R.id.image1);
+        image[3] = (ImageView) findViewById(R.id.image1);
+        image[4] = (ImageView) findViewById(R.id.image1);
+        image[5] = (ImageView) findViewById(R.id.image1);
+        image[6] = (ImageView) findViewById(R.id.image1);
+        image[7] = (ImageView) findViewById(R.id.image1);
+        image[8] = (ImageView) findViewById(R.id.image1);
+        image[9] = (ImageView) findViewById(R.id.image1);
+        image[10] = (ImageView) findViewById(R.id.image1);
+
+
+        toggle[0].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
                 if (isChecked) {
-                    image.setVisibility(View.INVISIBLE);
+                    image[0].setVisibility(View.VISIBLE);
                     // The toggle is enabled
                 } else {
-                    image.setVisibility(View.VISIBLE);
-                    //toggle.setVisibility(View.INVISIBLE);
+                    image[0].setVisibility(View.INVISIBLE);
                     // The toggle is disabled
                 }
             }
         });
+        toggle[1].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[1].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[1].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[2].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[2].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[2].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[3].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[3].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[3].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[4].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[4].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[4].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[5].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[5].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[5].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[6].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[6].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[6].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[7].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[7].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[7].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[8].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[8].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[8].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[9].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[9].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[9].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+        toggle[10].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    image[10].setVisibility(View.VISIBLE);
+                    // The toggle is enabled
+                } else {
+                    image[10].setVisibility(View.INVISIBLE);
+                    // The toggle is disabled
+                }
+            }
+        });
+
+
     }
 }
